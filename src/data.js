29ef37a0dc48bd0c7d7fe1e15520960f6fb02d90
bsr4Ks <basaraksu03@gmail.com>
2024-02-01 20:06:34 +0300

    modify data.js

--- src/data.js
+++ src/data.js
@@ -85,4 +85,18 @@ export default [
     location: "Online",
     openSpots: 0,
   },
+  {
+    id: 1,
+    title: "Life Lessons with Katie Zaferes",
+    description:
+      'I will share with you what I call "Positively Impactful Moments of Disappointment." Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.',
+    price: 136,
+    coverImg: "https://clonebnb-experience.vercel.app/images/katie-zaferes.png",
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
+    },
+    location: "Online",
+    openSpots: 0,
+  },
 ];
